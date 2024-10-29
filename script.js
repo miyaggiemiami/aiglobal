9eecac5bbecf682b8629c5ee71765b3d0a108ea2
miyaggiemiami <clementmorare971@gmail.com>
2024-10-29 23:59:08 +0200

    new changes

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 
 let isActive = false;
-function toggleActive() {
-  const hamburger = document.querySelector('.hamburger');
-  isActive = !isActive;
-  if (isActive) {
-    hamburger.classList.add('active');
-  } else {
-    hamburger.classList.remove('active');
-  }
-}
+
+    function handleToggle() {
+        isActive = !isActive;
+        const navList = document.querySelector('ul');
+        const hamburger = document.querySelector('.hamburger');
+        
+        // Toggle the 'active' class for the hamburger and the ul
+        if (isActive) {
+            navList.classList.add('active');
+            hamburger.classList.add('active');
+        } else {
+            navList.classList.remove('active');
+            hamburger.classList.remove('active');
+        }
+    }
